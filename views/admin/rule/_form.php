5689d22a007a966372f157c4d2ee9a4d54acf79f
Andrew <andrew.panix@gmail.com>
2019-09-16 19:24:46 +0300

    init

--- views/admin/rule/_form.php
+++ views/admin/rule/_form.php
@@ -8,27 +8,24 @@ use panix\engine\bootstrap\ActiveForm;
 /* @var $form ActiveForm */
 ?>
 <?php $form = ActiveForm::begin(); ?>
-<div class="row">
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-body">
-                <div class="rule-item-form">
-                    <?php echo $form->field($model, 'name')->textInput(['maxlength' => 64]); ?>
-                    <?php echo $form->field($model, 'className')->textInput(); ?>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="rule-item-form">
+                        <?= $form->field($model, 'name')->textInput(['maxlength' => 64]); ?>
+                        <?= $form->field($model, 'className')->textInput(); ?>
+                    </div>
                 </div>
-            </div>
-            <div class="card-footer text-center">
-                <?php echo Html::submitButton(
-                    $model->getIsNewRecord() ? Yii::t('rbac/default', 'Create') : Yii::t('rbac/default', 'Update'),
-                    [
-                        'class' => $model->getIsNewRecord() ? 'btn btn-success' : 'btn btn-primary',
-                    ]
-                ); ?>
+                <div class="card-footer text-center">
+                    <?= Html::submitButton(
+                        Yii::t('app', $model->getIsNewRecord() ? 'CREATE' : 'UPDATE'),
+                        [
+                            'class' => $model->getIsNewRecord() ? 'btn btn-success' : 'btn btn-primary',
+                        ]
+                    ); ?>
+                </div>
             </div>
         </div>
     </div>
-    <div class="col-md-6">
-        dsadsadsa
-    </div>
-</div>
 <?php ActiveForm::end(); ?>
